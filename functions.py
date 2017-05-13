5e27100e8387bc6a834d782107b06f1ad91a8978
helgaKalicz <kalicz.helga@gmail.com>
2017-05-13 23:50:35 +0200

    super-sprinter done

---
+++ functions.py
@@ -0,0 +1,60 @@
+def read_file(filename):
+    '''
+    Opens and reads a file, then returns a nested list with the elements of the file.
+    '''
+    with open(filename) as file:
+        file_list = [lines.split(',') for lines in file.read().splitlines()]
+    return file_list
+
+
+def write_file(filename, nestedlist):
+    '''
+    Rewrites the whole file.
+    '''
+    with open(filename, 'w') as file:
+            for items in nestedlist:
+                file.write(','.join(items) + '\n')
+
+
+def restore_comma(nestedlist):
+    '''
+    Restore the commas to the strings.
+    '''
+    for i in range(len(nestedlist)):
+        for j in range(len(nestedlist[i])):
+            if '¤' in nestedlist[i][j]:
+                nestedlist[i][j] = nestedlist[i][j].replace('¤', ',')
+    return nestedlist
+
+
+def convert_string(string):
+    '''
+    Replaces some elements in the string.
+    '''
+    string = string.replace('\r\n', ' ')
+    string = string.replace('¤', '')
+    string = string.replace(',', '¤')
+    string = ' '.join(string.split())
+    return string
+
+
+def valid_value(value):
+    '''
+    Returns True if the value is valid, else returns False.
+    '''
+    try:
+        value = int(value)
+        return False if value < 100 or value > 1500 or value % 100 != 0 else True
+    except:
+        return False
+
+
+def valid_time(time):
+    '''
+    Returns True if the time is valid, else returns False.
+    '''
+    try:
+        time = float(time)
+        return False if time < 0.5 or time > 40 or time % 0.5 != 0 else True
+    except:
+        return False
---
+++ main.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, render_template, redirect
+import functions
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/list')
+def listing():
+    '''
+    Lists all stories from the file.
+    '''
+    story_list = functions.restore_comma(functions.read_file('result.txt'))
+    return render_template('list.html', file_list=story_list)
+
+
+@app.route('/story', methods=['GET'])
+def a_new_story():
+    '''
+    Allows to fill an empty form out with a story.
+    '''
+    return render_template('form.html', story=None)
+
+
+@app.route('/story', methods=['POST'])
+def add_new_story():
+    '''
+    Writes the new story to the file, based on the user inputs.
+    '''
+    if functions.valid_value(request.form['business_value']) and functions.valid_time(request.form['estimation_time']):
+        with open('result.txt', 'a') as file:
+            file.write(str(int(functions.read_file('result.txt')[-1][0]) + 1) + ',')
+            file.write(str(functions.convert_string(request.form['story_title'])) + ',')
+            file.write(str(functions.convert_string(request.form['user_story'])) + ',')
+            file.write(str(functions.convert_string(request.form['acceptance_criteria'])) + ',')
+            file.write(str(request.form['business_value']) + ',')
+            file.write(str(request.form['estimation_time']) + 'h,')
+            file.write(str(request.form['status']) + '\n')
+        return redirect('/list')
+    else:
+        return render_template('error.html')
+
+
+@app.route('/story/<int:story_id>', methods=['GET'])
+def show_story(story_id):
+    '''
+    Redirects to a page where the user can change the elements of the chosen story.
+    '''
+    story_list = functions.restore_comma(functions.read_file('result.txt'))
+    selected_story = ''
+    for story in story_list:
+        if int(story[0]) == int(story_id):
+            selected_story = story
+            break
+    return render_template('form.html', story=selected_story, story_id=story_id)
+
+
+@app.route('/story/<int:story_id>', methods=['POST'])
+def modify_story(story_id):
+    '''
+    Saves the changes to the file, based on the user inputs.
+    '''
+    if functions.valid_value(request.form['business_value']) and functions.valid_time(request.form['estimation_time']):
+        story_list = functions.read_file('result.txt')
+        for story in story_list:
+            if int(story[0]) == int(story_id):
+                story[1] = str(functions.convert_string(request.form['story_title']))
+                story[2] = str(functions.convert_string(request.form['user_story']))
+                story[3] = str(functions.convert_string(request.form['acceptance_criteria']))
+                story[4] = str(request.form['business_value'])
+                story[5] = str(request.form['estimation_time'])
+                story[6] = str(request.form['status'])
+                break
+        functions.write_file('result.txt', story_list)
+        return redirect('/list')
+    else:
+        return render_template('error.html')
+
+
+@app.route('/story/<int:story_id>/delete', methods=['POST'])
+def delete_story(story_id):
+    '''
+    Deletes the chosen story from the file.
+    '''
+    story_list = functions.read_file('result.txt')
+    for story in story_list:
+        if int(story[0]) == int(story_id):
+            story_list.remove(story)
+    functions.write_file('result.txt', story_list)
+    return redirect('/list')
+
+
+@app.errorhandler(404)
+def error_handling(e):
+    '''
+    Handles errors.
+    '''
+    return render_template('error.html')
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
